19e722d68ee19732d44b0f47d14e89c452f75487
Alazar Tadesse <mealazartadesse@gmail.com>
2022-03-14 11:56:31 +0300

    new controllers and models

---
+++ app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class BlogPostController extends Controller
+{
+
+    public function index()
+    {
+        $posts = BlogPost::all();
+        return $posts;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(BlogPost $blogPost)
+    {
+        return $blogPost;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BlogPost $blogPost)
+    {
+        //
+    }
+}
